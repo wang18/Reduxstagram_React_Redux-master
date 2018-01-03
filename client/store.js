d0afe06793c6968cba74f1279678dc288b6c9b7a
wang18 <wang18@uoregon.edu>
2018-01-03 14:48:19 -0800

    complete

---
+++ client/store.js
@@ -0,0 +1,20 @@
+import {createStore, compse} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+import rootReducer from './reducers/index';
+import comments from './data/comments';
+import posts from './data/posts';
+
+const defaultState={ posts,comments};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+if(module.hot){
+    module.hot.accept('./reducers', ()=>{
+        const nextRootReducer = require('./reducers/index').default;
+        store.replaceReducer(nextRootReducer);
+    });
+}
+export default store;
